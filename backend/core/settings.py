26ebbdc092d397567ed8a7e65cfe00e1f875d7fe
GabrielFBB <gabrielfbb03@gmail.com>
2026-08-19 23:52:19 +0100

    feat: JWT authentication working

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'workout',
     'reading',
     'habits',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,16 @@ MAILERS = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
